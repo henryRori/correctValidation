5c89239cbb5417305a034a1e03073620cef62930
henry <you@example.com>
2019-03-07 20:20:06 +0100

    firstvalidation

--- src/Components/Navbar/Home/Home.jsx
+++ src/Components/Navbar/Home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
             <h2>Welcome</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque repellat cum eveniet? 
                 Unde, obcaecati laudantium quas,
-                 nemo veritatis adipisci voluptate quidem asperiores consectetur sint amet?</p>
+                 nemo veritatis adipisci voluptate quidem asperiores consectetur sint amet? everything is fine</p>
         </div>
     );
 }
